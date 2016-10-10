7180fc1015a56c1c97a3ddd4f973f2ad0d20a685
unknown <>
2016-10-10 21:39:37 +0800

    stumansys

---
+++ TestInput.java
@@ -0,0 +1,20 @@
+package bookp35;
+
+import java.util.Scanner;
+
+public class TestInput {
+	public static void main(String[] args){
+		int age;
+		double weight;
+		Scanner scanner=new Scanner(System.in);
+		
+		System.out.println("请问你的年龄是多少岁？");
+		age=scanner.nextInt();
+		
+		System.out.println("请问你的体重是多少公斤？");
+		weight=scanner.nextDouble();
+		
+		System.out.println("你的年龄是"+age+"岁,体重是"+weight+"公斤。");
+	}
+
+}
